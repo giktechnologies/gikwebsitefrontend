5331efea48547f24bbe0a3abf70a9f4c365460e7
Ikay <ikaydevelopment@gmail.com>
2024-09-20 17:01:41 +0200

    services update icons, and components

---
+++ components/Services/index.tsx
@@ -0,0 +1,27 @@
+import SectionTitle from "../Common/SectionTitle";
+import SingleService from "./SingleService";
+import servicesData from  "./servicesData"
+
+const Services = () => {
+  return (
+    <>
+      <section id="features" className="py-16 md:py-20 lg:py-28">
+        <div className="container">
+          <SectionTitle
+            title="Main Services"
+            paragraph="There are many variations of passages of Lorem Ipsum available but the majority have suffered alteration in some form."
+            center
+          />
+
+          <div className="grid grid-cols-1 gap-x-8 gap-y-14 md:grid-cols-2 lg:grid-cols-3">
+            {servicesData.map((feature) => (
+              <SingleService key={feature.id} feature={feature} />
+            ))}
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Services;
