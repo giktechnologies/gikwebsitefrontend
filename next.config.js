397694fb9fbea946a5ca7d67b3af595797f83888
Ikay <ikaydevelopment@gmail.com>
2024-09-29 20:25:05 +0200

    added firebase configuaration

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
     domains: ["localhost"],
     remotePatterns: [
@@ -9,7 +10,39 @@ const nextConfig = {
         port: "",
       },
     ],
+    unoptimized: true,
   },
 };
 
 module.exports = nextConfig;
+
+// {
+//   "functions": [
+//     {
+//       "source": "functions",
+//       "codebase": "default",
+//       "ignore": [
+//         "node_modules",
+//         ".git",
+//         "firebase-debug.log",
+//         "firebase-debug.*.log",
+//         "*.local"
+//       ],
+//       "predeploy": [
+//         "npm --prefix \"$RESOURCE_DIR\" run lint",
+//         "npm --prefix \"$RESOURCE_DIR\" run build"
+//       ]
+//     }
+//   ],
+//   "hosting": {
+//     "public": "public",
+//     "ignore": [
+//       "firebase.json",
+//       "**/.*",
+//       "**/node_modules/**"
+//     ]
+//   },
+//   "database": {
+//     "rules": "database.rules.json"
+//   }
+// }
