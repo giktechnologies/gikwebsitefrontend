43de06412c67d0cccf5919a4fd484b109ac291a9
Ikay <ikaydevelopment@gmail.com>
2025-11-11 20:02:45 +0200

    removed line of old code

--- app/api/firebaseAdmin.ts
+++ app/api/firebaseAdmin.ts
@@ -1,29 +1,6 @@
-// firebase.ts
-// import * as admin from 'firebase-admin';
-// import { ServiceAccount } from 'firebase-admin/lib/app/credential';
-
-// // Load your service account key
-// const serviceAccount = require('gikwebsite-98444-firebase-adminsdk-fbsvc-59cc51d453.json') as ServiceAccount;
-
-// // Check if Firebase has already been initialized
-// if (!admin.apps.length) {
-//   admin.initializeApp({
-//     credential: admin.credential.cert(serviceAccount),
-//     databaseURL: 'https://gikwebsite-98444-default-rtdb.firebaseio.com/', // Replace with your Realtime Database URL
-//   });
-// }
-
-// // Export the Realtime Database instance
-// const db = admin.database();
-
-// export { db };
-
-
 import * as admin from 'firebase-admin';
-import { ServiceAccount } from 'firebase-admin/lib/app/credential';
 
 // Load your service account key
-const serviceAccount = require('gikwebsite-98444-firebase-adminsdk-fbsvc-59cc51d453.json') as ServiceAccount;
 const privateKey = process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n');
 // Check if Firebase has already been initialized
 if (!admin.apps.length) {
